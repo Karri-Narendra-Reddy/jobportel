7dbbead21aceb497942655d78b8e4e43a93b0693
Karri-Narendra-Reddy <narendrareddykarri11@gmail.com>
2022-03-11 22:23:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+//console.log('Hello');
+
+let name = 'Narendrahackz';
+console.log(name);
+
+//grouping values
+let firstName="karri",lastName = "Narendra" ;
+console.log(firstName,lastName);
+console.log(lastName);
+
+// using const
+const intrestRate = 0.5;
+console.log(intrestRate);
+
+//primitive Types
+let Name="Narendra";//string literal
+let age = 30; // Number literal
+let isApproved = true; //boolean literal
+let middleName = undefined; //undefined
+let centerName = null; //null
+let selectedColor = 'red' ;
+
+// how to use typeof in js 
+console.log(typeof Name);
+
+
+//  using "let"--->dynamic
+//  using string,int,float -----> static variables
+
+//object
+let person={
+    name: "kali",
+    age: 30
+};
+console.log(person);
+    //wana change name use dot notation or use brackets
+person.name = 'parrot';//dot notation
+person["age"]= 40;
+console.log('person');
+console.log(typeof person.age);
